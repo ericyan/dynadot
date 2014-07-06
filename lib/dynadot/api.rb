f5a71ce8dfab6cbc021095071c353c3dfa740051
Eric Yan <long@ericyan.me>
2014-07-07 00:20:09 +0800

    Execute commands

--- lib/dynadot/api.rb
+++ lib/dynadot/api.rb
@@ -9,5 +9,9 @@ module Dynadot
       raise ArgumentError, 'An API key is required' if key.to_s.empty?
       self.class.default_params key: key.to_s
     end
+
+    def execute(command, parameters = {})
+      self.class.get '', query: { command: command.to_s }.merge(parameters)
+    end
   end
 end
