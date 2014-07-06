c86e264ab6159e83a952c3decc3d333db69cad57
Eric Yan <long@ericyan.me>
2014-07-07 00:18:40 +0800

    Set API key when initializing

--- lib/dynadot/api.rb
+++ lib/dynadot/api.rb
@@ -4,5 +4,10 @@ module Dynadot
   class Api
     include HTTParty
     base_uri 'https://api.dynadot.com/api2.html'
+
+    def initialize(key = nil)
+      raise ArgumentError, 'An API key is required' if key.to_s.empty?
+      self.class.default_params key: key.to_s
+    end
   end
 end
--- spec/dynadot/api_spec.rb
+++ spec/dynadot/api_spec.rb
@@ -8,4 +8,10 @@ describe Dynadot::Api do
   it "should have the base url set to the Domain API endpoint" do
     expect(Dynadot::Api.base_uri).to eql 'https://api.dynadot.com/api2.html'
   end
+
+  describe "#new" do
+    it "requires a API key" do
+      expect { Dynadot::Api.new }.to raise_error(ArgumentError)
+    end
+  end
 end
